606c3d3335d5a5ca9b55f7f103f891a31be5cc84
Gulrez <sidd5312@gmailcom>
2022-11-14 18:14:42 +0530

    code formatted

--- pages/login.js
+++ pages/login.js
@@ -13,27 +13,28 @@ function Login() {
         auth.signInWithPopup(provider).catch(alert);
     }
     return (
-        <Container>
+        <div>
             <Head>
                 <title>
-                    Login Page hello
+                    Login Page helloworld
                 </title>
-                {/* <meta name="description" content="LineupX | 4609 followers on LinkedIn. On a mission to make recruitment easy for everybody | Lineupx is a recruitment platform that enables companies to hire" /> */}
+                <meta name="description" content="LineupX | 4609 followers on LinkedIn. On a mission to make recruitment easy for everybody | Lineupx is a recruitment platform that enables companies to hire" />
                 <meta name="theme-color" content="#008f68" />
                 <meta property="og:url" content="https://lineupx.com" />
                 <meta property="og:title" content="Lineupx" />
                 <meta property="og:type" key="og:type" content="website" />
                 <meta property="og:description" content="LineupX | 4609 followers on LinkedIn. On a mission to make recruitment easy for everybody | Lineupx is a recruitment platform that enables companies to hire" />
-                <meta property="og:image" content={vercel} />
-
+                <meta property="og:image" content={"https://vercel-og-nextjs-theta.vercel.app/api/dynamic-image?logourl=https://cdn.lineupx.com/images/logo/Icon-Fill-Black.png&title=helloworld&desc=2-3,4-5,mumbai-pune"} />
             </Head>
-            <LoginContainer>
-                <Logo
-                    src='http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png'
-                />
-            <Button onClick={signIn} variant='outlined'>Sign in with Google Now222</Button>
-            </LoginContainer>
-        </Container>
+            <Container>
+                <LoginContainer>
+                    <Logo
+                        src='http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png'
+                    />
+                    <Button onClick={signIn} variant='outlined'>Sign in with Google Now3333</Button>
+                </LoginContainer>
+            </Container>
+        </div>
     )
 }
 
