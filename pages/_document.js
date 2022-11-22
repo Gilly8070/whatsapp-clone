e777ac3a5c953d4b84003fa8559bda43b5a9634d
Gilly8070 <65252737+Gilly8070@users.noreply.github.com>
2022-11-22 18:26:36 +0530

    _document special page added for testing

---
+++ pages/_document.js
@@ -0,0 +1,38 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+export default class CustomDocument extends Document {
+  render() {
+  return (
+    <Html>
+    <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <meta
+      name="description"
+      content="Web site created using create-react-app"
+    />
+
+
+    <!--  Essential META Tags -->
+    <meta property="og:title" content="My Whatsapp Clone" />
+    <meta property="og:description"
+    content="lorem lipsum whatsapp " />
+    <meta property="og:url" content="https://legendary-gnome-12ddb6.netlify.app" />
+    <meta property="og:image"
+    content="https://www.seekpng.com/png/small/343-3434867_warrner-bros-logo-featured-warner-bros-logo-png.png" />
+    
+    <meta  name="twitter:card" content="summary" />
+    <meta property="fb:app_id" content="your_app_id" />
+    <!-- url not working in telegram -->
+    <meta property="og:site_name" content="https://legendary-gnome-12ddb6.netlify.app/">
+    <meta property="og:type" content="article" />
+    <meta name="description" content="lorem lipsum whatsapp22222" />
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+  } 
+}
