cbd760e23101bc271b99699de8554f27eada0a53
Gulrez  shaikh <gulrezshaikh@Gulrezs-MacBook-Air.local>
2023-05-07 14:10:43 +0530

    [Update]: _document file removed

---
+++ pages/logintesting/testdoc.js
@@ -0,0 +1,39 @@
+// import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+// export default class CustomDocument extends Document {
+//   render() {
+//     return (
+//       <Html>
+//         <Head>
+//           <meta name="viewport" content="width=device-width, initial-scale=1" />
+//           <meta name="theme-color" content="#000000" />
+//           <meta
+//             name="description"
+//             content="Web site created using create-react-app"
+//           />
+
+
+//           {/* <!--  Essential META Tags --> */}
+
+//           <meta property="og:title" content="My Whatsapp Clone" />
+//           <meta property="og:description"
+//             content="lorem lipsum whatsapp " />
+//           <meta property="og:url" content="https://legendary-gnome-12ddb6.netlify.app" />
+//           <meta property="og:image"
+//             content="https://s3.ap-south-1.amazonaws.com/cdn.lineupx.com/images/logo/Icon-Fill-Blue.png" />
+
+//           <meta name="twitter:card" content="summary" />
+//           <meta property="fb:app_id" content="your_app_id" />
+//           {/* <!-- url not working in telegram --> */}
+//           <meta property="og:site_name" content="https://legendary-gnome-12ddb6.netlify.app/" />
+//           <meta property="og:type" content="article" />
+//           <meta name="description" content="lorem lipsum whatsapp22222" />
+//         </Head>
+//         <body>
+//           <Main />
+//           <NextScript />
+//         </body>
+//       </Html>
+//     )
+//   }
+// }
